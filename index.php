73ff449349c5bab188bcbd815e95b45aca5f55cb
Ramon Wong <rcnwong78@gmail.com>
2022-05-28 14:42:00 -0300

    added index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>HTML 5</title>
+    <link rel="stylesheet" href="style.css">
+    
+  </head>
+  <body>
+	<script src="index.js"></script>
+	
+	
+	
+  </body>
+</html>
